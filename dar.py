2f32aa4e42b422692979b9be85293cdd3a7444e3
JoinikilXD <jonikil15@gmail.com>
2021-07-19 20:41:13 +0300

    Initial commit

---
+++ dar.py
@@ -0,0 +1,9 @@
+article = input("Введите газетную статью\n")
+if "Styopa" in article:
+    article2 = article.replace("Styopa", "Kirill")
+    print(article2)
+elif "Стёпа" in article:
+    article2 = article.replace("Стёпа", "Кирилл")
+    print(article2)
+else:
+    print("Стёпы здесь нет")
